8bd9a0326f1966d8ac22eb6b87939b977b966d6e
Samy Abouseda <samy.abouseda@gmail.com>
2018-02-27 07:26:13 +0100

    structure for the modeles created

---
+++ src/metier/ModeleListDisciplines.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author samyabouseda
+ */
+public class ModeleListDisciplines extends ListeObjects {
+    
+}
