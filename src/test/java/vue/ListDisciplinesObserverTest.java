0be511e0ee075dc13bfda14a14eeaf56b3f95f6e
Samy Abouseda <samy.abouseda@gmail.com>
2018-05-09 15:56:24 +0200

    Rename ListDisciplinesObserver

---
+++ src/test/java/vue/ListDisciplinesObserverTest.java
@@ -0,0 +1,59 @@
+package vue;
+
+import domaine.Sport;
+import java.awt.List;
+import java.util.ArrayList;
+import metier.Action;
+import metier.ListAthletes;
+import metier.ListDisciplines;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author samyabouseda
+ */
+public class ListDisciplinesObserverTest {
+    
+    private ListDisciplinesObserver observer;
+    private List listGUI;
+    private ListAthletes listAthletes;
+    private ListDisciplines listDisciplines;
+    private Action action;
+    
+    @BeforeTest
+    protected void setUp() {
+        listAthletes = new ListAthletes();
+        listDisciplines = new ListDisciplines();
+        listGUI = new List();
+        observer = new ListDisciplinesObserver(listGUI, listAthletes);
+    }
+    
+    @Test
+    public void should_have_empty_list_if_action_received_is_not_instanceof_action() {
+        listDisciplines.chargerDonnes();
+        observer.update(listDisciplines, "LOAD");
+        ArrayList list = new ArrayList();
+        for(int i = 0; i<listGUI.getItemCount(); i++) { list.add(listGUI.getItem(i)); } 
+        assertThat(list).isEmpty();
+    }
+    
+    @Test
+    public void should_have_filled_list_if_action_received_is_instanceof_action() {
+        action = new Action(Action.LOAD);
+        observer.update(listDisciplines, action);
+        int size = listGUI.getItemCount();
+        assertThat(size).isGreaterThan(0);
+    }
+    
+    @Test
+    public void should_have_paysCrt_if_action_received_is_sel() {
+        listDisciplines.setPos(0);
+        action = new Action(Action.SEL, listDisciplines.getPos());
+        observer.update(listDisciplines, action);
+        Sport sportCrt = listAthletes.getSportCrt();
+        assertThat(sportCrt).isNotNull();
+    }
+    
+}
