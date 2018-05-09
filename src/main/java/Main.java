94f609568fc4dffd80e3dd6c4bf75909e945cfdc
Samy Abouseda <samy.abouseda@gmail.com>
2018-05-09 16:39:45 +0200

    Remove logger from FrmMain

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-
-import java.util.logging.Logger;
-
 public class Main {
 
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Main.class.getName());
-        logger.info("Logger is working!");
         new vue.FrmMain().setVisible(true);
     }
 }
