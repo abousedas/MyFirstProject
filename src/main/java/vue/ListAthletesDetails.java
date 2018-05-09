38ecdfa0b89374b6f787234b1088ab0dc1a08a73
Samy Abouseda <samy.abouseda@gmail.com>
2018-05-09 17:35:49 +0200

    Add test for ListAthletesDetails

--- src/main/java/vue/ListAthletesDetails.java
+++ src/main/java/vue/ListAthletesDetails.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vue;
 
 import domaine.Athlete;
-import java.awt.List;
 import java.awt.TextField;
 import java.util.Observable;
 import java.util.Observer;
@@ -18,12 +12,18 @@ import metier.ListAthletes;
  * @author samyabouseda
  */
 public class ListAthletesDetails implements Observer {
-    TextField no, prenom, nom, natio, sport;
+    
+    private TextField no, prenom, nom, natio, sport;
+    
     public ListAthletesDetails(TextField no, TextField prenom, TextField nom, TextField natio, TextField sport) { 
         this.no=no; this.prenom=prenom; this.nom=nom; this.natio=natio; this.sport=sport;
     }
     
-    public void update(Observable m, Object args) {
+    public void update(Observable o, Object args) {
+        if (args instanceof Action) { update(o, (Action)args); }
+    }
+    
+    private void update(Observable m, Action args) {
         ListAthletes o = (ListAthletes)m;
         switch(((Action)args).getAction()) {
             case Action.SEL : 
---
+++ src/test/java/vue/ListAthletesDetailsTest.java
@@ -0,0 +1,72 @@
+package vue;
+
+import domaine.Pays;
+import domaine.Sport;
+import java.awt.TextField;
+import metier.Action;
+import metier.ListAthletes;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author samyabouseda
+ */
+public class ListAthletesDetailsTest {
+    
+    private ListAthletesDetails observer;
+    private ListAthletes listAthletes;
+    private TextField tfNo, tfPrenom, tfNom, tfNatio, tfSport;
+    private Action action;
+    private Pays pays;
+    private Sport sport;
+    
+    @BeforeTest
+    protected void setUp() {
+        tfNo = new TextField();
+        tfPrenom = new TextField();
+        tfNom = new TextField();
+        tfNatio = new TextField();
+        tfSport = new TextField();
+        pays = new Pays(34, "CAN", "Canada");
+        sport = new Sport(4, "Curling");
+        listAthletes = new ListAthletes();
+        listAthletes.setPaysCrt(pays);
+        listAthletes.setSportCrt(sport);
+        listAthletes.chargerAthletes();
+        observer = new ListAthletesDetails(tfNo, tfPrenom, tfNom, tfNatio, tfSport);
+    }
+    
+    @Test
+    public void textFields_should_be_empty_if_action_received_is_not_instanceof_action() {
+        observer.update(listAthletes, "SEL");
+        assertThat(tfNo.getText()).isEmpty();
+        assertThat(tfPrenom.getText()).isEmpty();
+        assertThat(tfNom.getText()).isEmpty();
+        assertThat(tfNatio.getText()).isEmpty();
+        assertThat(tfSport.getText()).isEmpty();
+    }
+    
+    @Test
+    public void textFields_should_be_filled_with_details_if_action_received_is_sel() {
+        action = new Action(Action.SEL, 0);
+        observer.update(listAthletes, action);
+        assertThat(tfNo.getText()).isNotEmpty();
+        assertThat(tfPrenom.getText()).isNotEmpty();
+        assertThat(tfNom.getText()).isNotEmpty();
+        assertThat(tfNatio.getText()).isNotEmpty();
+        assertThat(tfSport.getText()).isNotEmpty();
+    }
+    
+    @Test
+    public void textFields_should_be_empty_if_pos_received_is_negative() {
+        action = new Action(Action.SEL, -1);
+        observer.update(listAthletes, action);
+        assertThat(tfNo.getText()).isEmpty();
+        assertThat(tfPrenom.getText()).isEmpty();
+        assertThat(tfNom.getText()).isEmpty();
+        assertThat(tfNatio.getText()).isEmpty();
+        assertThat(tfSport.getText()).isEmpty();
+    }
+}
