31232166c9f60a0abdca835b536978b86102b2fa
Samy Abouseda <samy.abouseda@gmail.com>
2018-06-04 09:49:26 +0200

    Remove code smells from ListDisciplinesObserver

--- src/main/java/vue/ListDisciplinesObserver.java
+++ src/main/java/vue/ListDisciplinesObserver.java
@@ -31,6 +31,7 @@ public class ListDisciplinesObserver implements Observer {
         switch(args.getAction()) {
             case Action.LOAD: chargerDisciplines((ListDisciplines)o); break;
             case Action.SEL : selSport((ListDisciplines)o); break;
+            default: return;
         }
     }
     
